2f3c0dfae2e1330f5cb7bec5cfc3374ef305a191
Denis565 <isip_d.m.balaev@mpt.ru>
2021-11-07 01:03:04 +0300

    Создание формы авторизации

---
+++ src/main/java/com/example/fitnes/controllers/TrainingScheduleController.java
@@ -0,0 +1,10 @@
+package com.example.fitnes.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.persistence.Entity;
+
+@Entity
+@RequestMapping("/subscriptionsale")
+public class TrainingScheduleController {
+}
